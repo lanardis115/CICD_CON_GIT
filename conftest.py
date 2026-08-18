e5d266cbf4c9b448fe50b8f5710ff35d923c9be8
L.A.N. <l.a.nardis115@gmail.com>
2026-08-18 13:35:57 +0200

    fix: screenshot incorporato in base64 nel report html

--- conftest.py
+++ conftest.py
@@ -1,18 +1,24 @@
+import base64
 import pytest
+import pytest_html
 
 @pytest.hookimpl(hookwrapper=True)
 def pytest_runtest_makereport(item, call):
-    """Intercetta gli errori e allega uno screenshot direttamente dentro il report HTML."""
     outcome = yield
     report = outcome.get_result()
-        
+    extra = getattr(report, "extra", [])
+
     if report.when == "call" and report.failed:
         page = item.funcargs.get("page")
         if page:
-            screenshot_bytes = page.screenshot(full_page=True)
-            pytest_html = item.config.pluginmanager.getplugin("html")
-            if pytest_html:
-                extra = getattr(report, "extra", [])
-                html_img = f'<div><p><b>Screenshot Errore:</b></p><img src="data:image/png;base64,{screenshot_bytes.hex()}" style="width:600px; border:1px solid red;"/></div>'
-                extra.append(pytest_html.extras.html(html_img))
-                report.extra = extra
+            # 1. Cattura lo screenshot in memoria come byte
+            screenshot_bytes = page.screenshot()
+            
+            # 2. Converti i byte in una stringa Base64
+            screenshot_b64 = base64.b64encode(screenshot_bytes).decode("utf-8")
+            
+            # 3. Incorpora l'immagine direttamente nell'HTML
+            html_img = f'<div><p><b>Screenshot Errore:</b></p><img src="data:image/png;base64,{screenshot_b64}" style="width:600px; max-width:100%; border:1px solid red;"/></div>'
+            
+            extra.append(pytest_html.extras.html(html_img))
+            report.extra = extra
